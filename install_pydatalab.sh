4b4f401193bc837c0c866d6c0956146c4fab2fe8
Eyad Sibai <eyad.alsibai@gmail.com>
2017-01-06 22:12:04 +0100

    add google package

--- install_pydatalab.sh
+++ install_pydatalab.sh
@@ -2,3 +2,4 @@
 git clone https://github.com/googledatalab/pydatalab.git
 cd pydatalab
 ./install-virtualenv.sh  # For use in Python virtual environments
+# TODO should install with pip and not install its dependencies
