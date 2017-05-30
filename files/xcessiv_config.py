584530260b0b17be8d50c78b2b09f72e9ae142f7
Eyad Sibai <eyad.alsibai@gmail.com>
2017-05-30 16:06:33 +0200

    add xcessiv

---
+++ files/xcessiv_config.py
@@ -0,0 +1,6 @@
+REDIS_HOST = 'localhost'  # Host address of Redis server
+REDIS_PORT = 6379  # Port of Redis Server
+REDIS_DB = 8  # Redis database number to use
+
+XCESSIV_PORT = 1994  # Port at which to start the Xcessiv server
+NUM_WORKERS = 1  # Number of RQ workers to start
